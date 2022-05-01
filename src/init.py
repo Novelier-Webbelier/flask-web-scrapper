66108f5ebb5f7a6dff07ae1b15b6639ba4239b25
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-05-01 15:33:37 +0900

    #4.5 Faster Scrapper

--- src/init.py
+++ src/init.py
@@ -3,10 +3,12 @@ from scrapper import get_jobs
 
 app = Flask("Flask Web Scrapper")
 
+fake_db = {}
+
 # http://127.0.0.1:5000/
 @app.route("/")
 def home():
-    return render_template("potato.html")
+    return render_template("home.html")
 
 # http://127.0.0.1:5000/<username>
 @app.route("/<username>")
@@ -17,11 +19,20 @@ def contact(username):
 @app.route("/report")
 def report():
     word = request.args.get("word")
+
     if word:
         word = word.lower()
+        from_db = fake_db.get(word)
+
+        if from_db:
+            jobs = from_db
+        else:
+            jobs = get_jobs(word)
+            fake_db[word] = jobs
+
     else:
         return redirect("/")
 
-    return render_template("report.html", searchingBy=word)
+    return render_template("report.html", searchingBy=word, resultsNumber=len(jobs))
 
 app.run()
