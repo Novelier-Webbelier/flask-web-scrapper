87288ab91ecae63d108fa7706510ba98d1c8d3b2
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-05-01 14:41:54 +0900

    #4.3 Forms and Query Arguments

--- src/init.py
+++ src/init.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("Flask Web Scrapper")
 
+# http://127.0.0.1:5000/
 @app.route("/")
 def home():
     return render_template("potato.html")
 
+# http://127.0.0.1:5000/<username>
 @app.route("/<username>")
 def contact(username):
-    return f"Hello {username} how are you doing"
+    return f"Your name is {username}"
+
+# http://127.0.0.1:5000/report?job=react
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    return render_template("report.html", searchingBy=word)
 
 app.run()
